3b802a8d181ff14b3014664df97d8d3ea0055e88
Glorken <jackelofalltrades@gmail.com>
2017-01-21 16:57:03 -0500

    Updates OI and adds Tank and Arcade commands
    Adds DriveTrainTankDrive command
    Adds DriveTrain ArcadeDrive command
    Updates OI to include two joysticks and the option to choose teleop
    commands

--- src/org/usfirst/frc/team5957/robot/OI.java
+++ src/org/usfirst/frc/team5957/robot/OI.java
@@ -1,6 +1,12 @@
 package org.usfirst.frc.team5957.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+import org.usfirst.frc.team5957.robot.commands.DriveTrainArcadeDrive;
+import org.usfirst.frc.team5957.robot.commands.DriveTrainBrake;
+import org.usfirst.frc.team5957.robot.commands.DriveTrainTankDrive;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -11,9 +17,23 @@ public class OI {
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
-    Joystick stick = new Joystick(1);
-    // Button button = new JoystickButton(stick, buttonNumber);
+    public Joystick leftStick;
+    public Joystick rightStick;
+    Button tankButton;
+    Button arcadeButton;
+    Button brakeButton;
     
+    public OI() {
+    	leftStick = new Joystick(1);
+    	rightStick = new Joystick(2);
+        tankButton   = new JoystickButton(leftStick, 11);
+        arcadeButton = new JoystickButton(leftStick, 12);
+        brakeButton  = new JoystickButton(leftStick, 9);
+        
+        tankButton.whenPressed(new DriveTrainTankDrive());
+        arcadeButton.whenPressed(new DriveTrainArcadeDrive());
+        brakeButton.whenPressed(new DriveTrainBrake());
+    }
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
---
+++ src/org/usfirst/frc/team5957/robot/commands/DriveTrainArcadeDrive.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team5957.robot.commands;
+
+import org.usfirst.frc.team5957.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * A command that takes one joystick's vertical and horizontal values and
+ * drives the robot with them.
+ */
+public class DriveTrainArcadeDrive extends Command {
+
+    public DriveTrainArcadeDrive() {
+        // Use requires() here to declare subsystem dependencies
+    	requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.driveTrain.brake();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double forward = Robot.oi.leftStick.getY();
+    	double turn = Robot.oi.leftStick.getX();
+    	
+    	Robot.driveTrain.arcadeDrive(forward, turn);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.brake();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.driveTrain.brake();
+    }
+}
